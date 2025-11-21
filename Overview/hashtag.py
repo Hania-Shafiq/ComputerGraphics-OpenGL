765a475870750a8e39fa89f43d7959638339638b
Hania Shafiq <shafiqhania00@gmail.com>
2025-11-21 15:32:29 +0500

    Create hashtag.py
    different styles hashtag

---
+++ Overview/hashtag.py
@@ -0,0 +1,58 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+def drawHashtag():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    # -------- THIN LINE --------
+    glColor3f(0, 0, 1)  # Blue
+    glLineWidth(1)      # Thin
+    glBegin(GL_LINES)
+    glVertex2i(50, 150); glVertex2i(150, 150)
+    glVertex2i(50, 100); glVertex2i(150, 100)
+    glVertex2i(80, 50); glVertex2i(80, 200)
+    glVertex2i(120, 50); glVertex2i(120, 200)
+    glEnd()
+
+    # -------- THICK LINE --------
+    glColor3f(1, 0, 0)  # Red
+    glLineWidth(5)      # Thick
+    glBegin(GL_LINES)
+    glVertex2i(200, 150); glVertex2i(300, 150)
+    glVertex2i(200, 100); glVertex2i(300, 100)
+    glVertex2i(230, 50); glVertex2i(230, 200)
+    glVertex2i(270, 50); glVertex2i(270, 200)
+    glEnd()
+
+    # -------- STIPPLED LINE --------
+    glColor3f(0, 1, 0)  # Green
+    glLineWidth(2)
+    glEnable(GL_LINE_STIPPLE)
+    glLineStipple(1, 0x00FF)  # Dashed pattern
+    glBegin(GL_LINES)
+    glVertex2i(350, 150); glVertex2i(450, 150)
+    glVertex2i(350, 100); glVertex2i(450, 100)
+    glVertex2i(380, 50); glVertex2i(380, 200)
+    glVertex2i(420, 50); glVertex2i(420, 200)
+    glEnd()
+    glDisable(GL_LINE_STIPPLE)
+
+    glFlush()
+
+def myInit():
+    glClearColor(1, 1, 1, 0)  # White background
+    glColor3f(0, 0, 0)        # Default color
+    glPointSize(2)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 500, 0, 250)
+
+glutInit()
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize(500, 250)
+glutInitWindowPosition(100, 100)
+glutCreateWindow(b"Hashtag Lines")
+glutDisplayFunc(drawHashtag)
+myInit()
+glutMainLoop()
